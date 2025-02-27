1f5b5ac00e585db568a9b61d0f6a6e8f904a6a82
Jay Loew <jay.loew@progleasing.com>
2025-02-27 15:26:26 -0700

    add new RC environment and run tests green

--- utils/lowes.utils/urls.ts
+++ utils/lowes.utils/urls.ts
@@ -1,5 +1,6 @@
 // urls.ts
-let base: string = 'https://qa.approve.me/s/Lowes/122100#';
+// let base: string = 'https://qa.approve.me/s/Lowes/122100#';
+let base: string = 'https://rc.approve.me/s/Lowes/122424#/'
 export default {
     base: {
         base: base,
